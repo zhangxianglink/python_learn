91877e61cdabaaa38f7acf00a3bdc8eb11ae90f0
xiang z <zhangxianglink@163.com>
2023-03-01 22:34:26 +0800

    练习8

---
+++ test.py
@@ -0,0 +1,8 @@
+# 字面量
+print("hello world !!!!!!!")
+print(521)
+print(13.14)
+
+"""
+多行注释
+"""
---
+++ 类型.py
@@ -0,0 +1,6 @@
+name = "张三"
+age = 26
+money = 1000000.9
+print(type(name))
+print(type(age))
+print(type(money))
