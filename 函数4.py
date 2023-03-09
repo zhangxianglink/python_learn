4a7b62a08a24ee249a3930a85b5b85f9d5749dbc
will-nuc <zhangxianglink@163.com>
2023-03-09 22:55:16 +0800

    23

--- 函数4.py
+++ 函数4.py
@@ -8,9 +8,24 @@ def subtract(a, b):
     print(f"{a} - {b}")
     return a - b
 
-def multiply(a,b):
+
+def multiply(a, b):
     print(f"MULTIPLY {a} * {b}")
     return a * b
 
-def divide(a,b):
+
+def divide(a, b):
     print(f"DIVIDING {a} / {b}")
+    return a / b
+
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"{age} , {height} , {weight} , {iq}")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That become: ", what, "Can you do it")
