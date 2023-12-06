7a27104033e3d752f17207f8bdbc2606c311b41a
zhangxiang <zhangxiang@npnets.com>
2023-12-06 11:22:47 +0800

    fast api

---
+++ hard.way.for3/api/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    a = "a"
+    b = "b" + a
+    return {"hello world": b}
+
+
+
