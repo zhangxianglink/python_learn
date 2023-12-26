72f62b9986c1b87dee6774dd9a5d504a9ee05f6c
zhangxiang <zhangxiang@npnets.com>
2023-12-26 15:14:30 +0800

    学习设置conda，环境配置

--- hard.way.for3/api/main.py
+++ hard.way.for3/api/main.py
@@ -12,4 +12,6 @@ def root():
     return {"hello world": b}
 
 
+if __name__ == '__main__':
+    uvicorn.run(app='main:app',port=8888)
 
--- hard.way.for3/函数3.py
+++ hard.way.for3/函数3.py
@@ -3,15 +3,19 @@
 from sys import argv
 script, input_file = argv
 
+
 def _print_all(f):
     print(f.read())
+
 
 # 重置到第0个字节
 def rewind(f):
     f.seek(0)
 
+
 def print_a_line(line_count,f):
     print(line_count, f.readline())
+
 
 current_file = open(input_file)
 _print_all(current_file)
